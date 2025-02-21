1631ed17121f19dad28a0870aac897dc9e85efcf
JackH01 <jackhicks01@outlook.com>
2025-02-21 13:29:42 +0000

    Added logout functionality

--- to_do_app/to_do_app/settings.py
+++ to_do_app/to_do_app/settings.py
@@ -123,6 +123,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = "/"
-LOGIN_URL = 'login'
-LOGOUT_URL = 'logout'
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"
