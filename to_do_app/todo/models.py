a608b37a7a66cd409752bf38305eb054e733b7d5
JackH01 <jackhicks01@outlook.com>
2025-03-05 11:49:21 +0000

    Added initial ToDo and Task models

--- to_do_app/todo/models.py
+++ to_do_app/todo/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class ToDo(models.Model):
+    title = models.CharField(max_length=255)
+    desc = models.CharField(max_length=2550)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # Each ToDo can be reordered either:
+    # ... manually
+    position = models.IntegerField() 
+    # ... or automatically
+    dateCreated = models.DateField(auto_now_add=True)
+    # Should be the same as the task that was last modified.
+    lastModified = models.DateField() 
+
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    belongsTo = models.ForeignKey(ToDo, on_delete=models.CASCADE)
+    done = models.BooleanField()
+    dateCreated = models.DateField(auto_now_add=True)
+    lastModified = models.DateField(auto_now=True)
